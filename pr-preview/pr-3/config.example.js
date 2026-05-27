a761bff71dd4183da5768d32159bd02cab6605cf
NISHAN-AKALANKA <NISHAN-AKALANKA@users.noreply.github.com>
2026-05-27 14:05:17 +0000

    Deploy preview for PR 3 🛫

---
+++ pr-preview/pr-3/config.example.js
@@ -0,0 +1,23 @@
+window.DOCSHARE_CONFIG = {
+  appTitle: 'Your Office Name',
+  appSubtitle: 'Letter Management System',
+  appBranch: 'Office Branch',
+  copyrightText: '2026 Office',
+  driveUrl: '',
+  gasWebAppUrl: 'https://script.google.com/macros/s/YOUR_APPS_SCRIPT_DEPLOYMENT_ID/exec',
+  localStorageKey: 'your_office_lms_v1',
+  sessionStorageKey: 'your_office_sess_v1',
+  outgoingFrom: 'Your Office Name',
+  refPrefix: 'YOUR_REF_PREFIX',
+};
+DOCSHARE_APP_TITLE
+DOCSHARE_APP_SUBTITLE
+DOCSHARE_APP_BRANCH
+DOCSHARE_COPYRIGHT_TEXT
+DOCSHARE_DRIVE_URL
+DOCSHARE_GAS_WEB_APP_URL
+DOCSHARE_LOCAL_STORAGE_KEY
+DOCSHARE_SESSION_STORAGE_KEY
+DOCSHARE_OUTGOING_FROM
+DOCSHARE_REF_PREFIX
+PAGES_BASE_URL
